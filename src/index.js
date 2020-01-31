0c48907b9d2f98db2d74c8d87d52ee5c21c5179c
Andrey Fedack <andrey.fedack@gmail.com>
2020-01-31 18:39:13 +0200

    Updated build

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 function getCurrentVerison() {
-  // const {version} = require(path.resolve(process.env.GITHUB_WORKSPACE, 'package.json'));
-  console.log(path.resolve(process.env.GITHUB_WORKSPACE, 'package.json'))
+  const {version} = require(path.join(process.env.GITHUB_WORKSPACE, 'package.json'));
+  console.log(path.join(process.env.GITHUB_WORKSPACE, 'package.json'))
   // core.exportVariable("NPM_PKG_VERSION", version);
   return '1.0.1'
 }
 
 function getCurrentTag() {
   const ref = github.context.ref;
+  console.log('Ref: ', ref);
   const tagPath = "refs/tags/";
   if (ref && ref.startsWith(tagPath)) {
     const tag = ref.substr(tagPath.length, ref.length);
