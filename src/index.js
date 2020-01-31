1826811452f21c2c7c147b3611b763ec7ddb720d
Andrey Fedack <andrey.fedack@gmail.com>
2020-01-31 19:00:40 +0200

    Updated build

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 const path = require('path')
 const core = require('@actions/core');
-const github = require('@actions/github');
+const {GitHub, context} = require('@actions/github');
 
 function getCurrentVerison() {
   const {version} = require(path.join(process.env.GITHUB_WORKSPACE, 'package.json'));
-  console.log(path.join(process.env.GITHUB_WORKSPACE, 'package.json'))
-  // core.exportVariable("NPM_PKG_VERSION", version);
-  return '1.0.1'
+  core.exportVariable("NPM_PKG_VERSION", version);
+  return version
 }
 
-function getCurrentTag() {
-  const ref = github.context.ref;
-  console.log('Ref: ', ref);
-  const tagPath = "refs/tags/";
-  if (ref && ref.startsWith(tagPath)) {
-    const tag = ref.substr(tagPath.length, ref.length);
-    core.exportVariable("GITHUB_TAG_NAME", tag);
-    return tag
+async function getCurrentRelease() {
+  octokit = new GitHub(process.env.GITHUB_TOKEN);
+  const { owner, repo } = context.repo;
+  const release = await octokit.getLatestRelease({
+    owner,
+    repo
+  });
+  core.exportVariable("NPM_CURRENT_VERSION", version);
+  return release;
+}
+
+(async () => {
+  try {
+    const currentRelease = await getCurrentRelease();
+    const curenttVersion = getCurrentVerison();
+
+    console.log('Current tag: ',currentTag);
+    console.log('Current version: ', currentVersion);
+    console.log('Samever: ', currentVersion === currentTag);
+
+  } catch (error) {
+    core.setFailed(error.message);
   }
-}
+})()
 
-try {
-  const currentTag = getCurrentTag();
-  const curenttVersion = getCurrentVerison();
-
-  console.log('Current tag: ',currentTag);
-  console.log('Current version: ', currentVersion);
-  console.log('Samever: ', currentVersion === currentTag);
-
-} catch (error) {
-  core.setFailed(error.message);
-}
